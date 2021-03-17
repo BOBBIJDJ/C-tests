e0f312198ca9eb68f1421e57c36a183bfd303749
BOBBIJDJ <76691110+BOBBIJDJ@users.noreply.github.com>
2021-03-17 13:06:51 +0100

    changes

--- c-files/hello.c
+++ c-files/hello.c
@@ -2,24 +2,15 @@
 
 int main(){
 
-    //variable definition
-    float a;
-    float b;
-    float result;
+    int a;
 
-    printf("inserisci il primo numero:\n");
-    scanf("%f",&a); //scanning for input to put in variable "a"
+    scanf("%d",&a);
 
-    printf("inserisci il secondo numero:\n");
-    scanf("%f",&b); //scanning for te input to put in "b"
+    if (a == "%c" || a == "%s"){
 
-    //result value definition
+        printf("errore");
 
-    result = a + b;
-
-    //printing the final result
-    printf("the result is: %.2f + %.2f = %.2f", a, b, result);
-    //%.2f = %f stands for float and .2 define how many numbers there will be on the right behind the dot 
+    }
 
     return 0;
 }
--- c-files/quadrato.c
+++ c-files/quadrato.c
@@ -4,8 +4,8 @@ int main()
 {
     int i = 0;
 
-    char * a = "*";
-    char * b = "*        *";
+    string a = "*";
+    string b = "*        *";
 
     for (i=0;i<10;i++)
     {
